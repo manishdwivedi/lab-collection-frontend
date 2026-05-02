04b63af5b7a4e1fc386b98facca9d02672a8c02a
manishdwivedi <manishkrdwivedi2011@gmail.com>
2026-05-02 17:28:53 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //console.log('API baseURL:', process.env.REACT_APP_API_URL);
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL || ''}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL || ''}`,
   withCredentials: true,
 });
 
